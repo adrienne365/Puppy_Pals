5c9bf250f4ef90ab62bab1ff346c04e03ddce311
adrienne365 <adrienne365@gmail.com>
2023-09-04 14:58:00 -0400

    puppyList complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
